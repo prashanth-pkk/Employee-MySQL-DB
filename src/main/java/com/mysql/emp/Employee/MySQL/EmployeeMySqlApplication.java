d4b732078b06cb720583e3111546986daa5540f6
prashanth.kanukuntla <prashanth.kanukuntla@italentdigital.com>
2024-11-20 22:42:11 +0530

    mysql db operations

---
+++ src/main/java/com/mysql/emp/Employee/MySQL/EmployeeMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.mysql.emp.Employee.MySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysql/emp/Employee/MySQL/EmployeeMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysql.emp.Employee.MySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
